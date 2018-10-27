ee504654aa175061a6d37854ac13695a7d3a6667
Gabriel Catice <Gabriel Catice@DESKTOP-9PU8K8N>
2018-10-27 14:54:50 -0200

    Create car model

---
+++ RentidaCar2/RentidaCar2/Model/Vehicle.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentidaCar2.Model
+{
+    class Vehicle
+    {
+        private string id;
+        private string modelName;
+        private Brand carBrand;
+        private DateTime releaseYear;
+        private int kmAmount;
+        private Type carType;
+        private bool isRented = false;
+        private DateTime lastRentDate;
+
+        public Vehicle(string id, string modelName, Brand carBrand, DateTime releaseYear, int kmAmount, Type carType, bool isRented, DateTime lastRentDate)
+        {
+            this.id = id;
+            this.modelName = modelName;
+            this.carBrand = carBrand;
+            this.releaseYear = releaseYear;
+            this.kmAmount = kmAmount;
+            this.carType = carType;
+            this.isRented = isRented;
+            this.lastRentDate = lastRentDate;
+        }
+
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string ModelName
+        {
+            get
+            {
+                return modelName;
+            }
+
+            set
+            {
+                modelName = value;
+            }
+        }
+
+        internal Brand CarBrand
+        {
+            get
+            {
+                return carBrand;
+            }
+
+            set
+            {
+                carBrand = value;
+            }
+        }
+
+        public DateTime ReleaseYear
+        {
+            get
+            {
+                return releaseYear;
+            }
+
+            set
+            {
+                releaseYear = value;
+            }
+        }
+
+        public int KmAmount
+        {
+            get
+            {
+                return kmAmount;
+            }
+
+            set
+            {
+                kmAmount = value;
+            }
+        }
+
+        internal Type CarType
+        {
+            get
+            {
+                return carType;
+            }
+
+            set
+            {
+                carType = value;
+            }
+        }
+
+        public bool IsRented
+        {
+            get
+            {
+                return isRented;
+            }
+
+            set
+            {
+                isRented = value;
+            }
+        }
+
+        public DateTime LastRentDate
+        {
+            get
+            {
+                return lastRentDate;
+            }
+
+            set
+            {
+                lastRentDate = value;
+            }
+        }
+
+        public enum Brand
+        {
+            Chevrolet = 1,
+            Volkswagen = 2,
+            Ford = 3,
+            Fiat = 4
+        }
+
+        public enum Type
+        {
+            Simple = 1,
+            Complete = 2,
+            Premmium = 3
+        }
+    }
+}
