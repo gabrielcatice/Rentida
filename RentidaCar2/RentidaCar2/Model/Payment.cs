c84d42b3b60171f72c5af8b9aba1dad7ae59472a
Gabriel Catice <Gabriel Catice@DESKTOP-9PU8K8N>
2018-10-27 15:16:52 -0200

    Payment Model

---
+++ RentidaCar2/RentidaCar2/Model/Payment.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentidaCar2.Model
+{
+    class Payment
+    {
+        private string id;
+        private string methodName;
+
+        public Payment(string id, string methodName)
+        {
+            this.Id = id;
+            this.MethodName = methodName;
+        }
+
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string MethodName
+        {
+            get
+            {
+                return methodName;
+            }
+
+            set
+            {
+                methodName = value;
+            }
+        }
+    }
+}
