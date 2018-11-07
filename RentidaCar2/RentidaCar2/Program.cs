0f377f66a474a6a35510646451d64c28e30b8b9d
Gabriel Catice <Gabriel Catice@DESKTOP-9PU8K8N>
2018-11-07 21:35:39 -0200

    adding database fellas

--- RentidaCar2/RentidaCar2/Program.cs
+++ RentidaCar2/RentidaCar2/Program.cs
@@ -14,20 +14,9 @@ namespace RentidaCar2
         /// The main entry point for the application.
         /// </summary>
 
-        [DllImport("kernel32.dll")]
-        static extern bool AttachConsole(int dwProccessId);
-        private const int ATTACH_PARENT_PROCESS = -1;
-
         [STAThread]
         static void Main()
         {
-
-            AttachConsole(ATTACH_PARENT_PROCESS);
-
-            ClientAddress address = new ClientAddress("1", ClientAddress.AddressType.Road, "aaaaa", "123", "a", "a", "a", "a", "a");
-            Client client = new Client("1", "fullName", "phone", address, Client.Gender.Male, "", DateTime.Today, DateTime.Today, Client.ClientStatus.Regular);
-            Console.WriteLine(client.Address.AdddressName);
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Home());
