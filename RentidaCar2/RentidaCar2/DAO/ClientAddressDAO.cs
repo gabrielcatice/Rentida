16da7d6f66d8cdfbbad1afb4050b896b5ffd4b0c
Gabriel Catice <Gabriel Catice@DESKTOP-9PU8K8N>
2018-11-27 20:04:40 -0200

    client address dao

--- RentidaCar2/RentidaCar2/DAO/ClientAddressDAO.cs
+++ RentidaCar2/RentidaCar2/DAO/ClientAddressDAO.cs
@@ -14,7 +14,8 @@ namespace RentidaCar2.DAO
         public void Create(ClientAddress client)
         {
             Database.Database rentida = Database.Database.GetInstance();
-            string query = string.Format("", client.Id);
+            string query = string.Format("INSERT INTO rentida.client_address(address_type, address_name, address_number, neighbourhood, zip_code, city, state, country) VALUES('{0}','{1}','{2}','{3}','{4}','{5}','{6}')",
+                 client.ClientAddressType, client.AdddressName, client.AddressNumber, client.Neighbourhood, client.ZipCode, client.City, client.State, client.Country);
 
             rentida.ExecuteNonQuery(query);
         }
@@ -22,24 +23,39 @@ namespace RentidaCar2.DAO
         public ClientAddress Read(string id)
         {
             Database.Database rentida = Database.Database.GetInstance();
-            string query = "SELECT * FROM aluno WHERE cpf=" + id;
+            string query = "SELECT * FROM rentida.client_address WHERE id=" + id;
             DataSet ds = rentida.ExecuteQuery(query);
             ClientAddress address = new ClientAddress();
 
             DataRow dr = ds.Tables[0].Rows[0];
             address.Id = dr["id"].ToString();
+            address.ClientAddressType = ClientAddress.AddressType.Road;
+            address.AdddressName = dr["address_name"].ToString();
+            address.AddressNumber = dr["address_number"].ToString();
+            address.Neighbourhood = dr["neighbourhood"].ToString();
+            address.ZipCode = dr["zip_code"].ToString();
+            address.City = dr["city"].ToString();
+            address.State = dr["state"].ToString();
+            address.Country = dr["country"].ToString();
 
             return address;
         }
 
         public void Update(ClientAddress address)
         {
+            Database.Database rentida = Database.Database.GetInstance();
+            string query = string.Format("UPDATE rentida.client_address SET address_type='{0}', address_name='{1}', address_number='{2}', neighbourhood='{3}', zip_code='{4}', city='{5}', state='{6}', country='{7}'" + "WHERE id ='{7}'",
+                 address.ClientAddressType, address.AdddressName, address.AddressNumber, address.Neighbourhood, address.ZipCode, address.City, address.State, address.Country, address.Id);
 
+            rentida.ExecuteNonQuery(query);
         }
 
         public void Delete(string id)
         {
+            Database.Database rentida = Database.Database.GetInstance();
+            string query = string.Format("DELETE from rentida.client_address WHERE id =" + id);
 
+            rentida.ExecuteNonQuery(query);
         }
     }
 }
